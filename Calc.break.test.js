0d82495520414924788ca839af73cf792d704ff3
ben99933 <yangben999@gmail.com>
2025-10-22 20:20:12 +0800

    fix(ci): restore build by fixing failing test

--- Calc.break.test.js
+++ Calc.break.test.js
@@ -2,6 +2,7 @@ const Calc = require('./Calc');
 
 describe('Intentional failure demo', () => {
   test('this should fail', () => {
-    expect(1 + 1).toBe(3); // 故意寫錯，讓 CI 失敗
+    // expect(1 + 1).toBe(3); // 故意寫錯，讓 CI 失敗
+    expect(1 + 1).toBe(2);
   });
 });
